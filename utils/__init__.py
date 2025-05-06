204deede1606284777489f9eacb9a01e835dbd17
rubikdesign <adrian.costake@gmail.com>
2025-05-06 16:59:24 +0300

    changes

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Utility modules for FusionFrame application."""
-
-import logging
-
-# Set up module logger
-logger = logging.getLogger(__name__)
-
-# NU importa modulele aici pentru a evita importurile circulare
-# Codul original era: from . import face_utils, io_utils
